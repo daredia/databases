4b88e117af3b2970282f29a153a20dc6e0c3123f
daredia <shehzad.daredia@gmail.com>
2016-07-02 15:29:07 -0700

    Add client code and begin refactor

--- server/routes.js
+++ server/routes.js
@@ -10,6 +10,9 @@ router.get('/users', controller.users.get);
 
 router.post('/users', controller.users.post);
 
+// TODO: add route for / so that it loads up chatterbox client index.html
+// TODO: inspect get request fetch method data
+
 
 module.exports = router;
 
